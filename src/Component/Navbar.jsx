e8ae90e01003108ec5a44e52847e32e06bcb105d
alsonrova <alsonrovarja@gmail.com>
2024-02-22 03:36:00 +0300

    rounded

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 function Navbar() {
   return (
     <div className="w-full h-6 flex items-center justify-center gap-5">
-       <NavLink to={"/"} className={"font-semibold text-xl hover:bg-white/30 hover:backdrop-brightness-50 hover:backdrop-blur-xl rounded p-1 transition-all"}>
+       <NavLink to={"/"} className={"font-semibold text-xl hover:bg-white/30 hover:backdrop-brightness-50 hover:backdrop-blur-xl rounded-full p-1 transition-all"}>
           Home
        </NavLink>
        <NavLink to={"/project"} className={"font-semibold text-xl hover:bg-white/30 hover:backdrop-brightness-50 hover:backdrop-blur-xl rounded p-1 transition-all"}>
