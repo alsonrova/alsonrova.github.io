26cea48804d0e391e5e99f13576b45c54e849bd9
alsonrova <alsonrovarja@gmail.com>
2024-10-29 10:47:39 +0300

    fix project image bug

--- src/Component/ProjectPage/ProjectList.jsx
+++ src/Component/ProjectPage/ProjectList.jsx
@@ -1,12 +1,10 @@
 import ProjectCard from "./ProjectCard"
-import previewInvestLocatif from "../../assets/investlocatif.png"
-import previewMathPrepa from "../../assets/mathprepa.png"
 
 function ProjectList() {
   return (
     <div className="flex flex-wrap lg:p-20 p-10 gap-20 justify-center">
-        <ProjectCard title="Investissement Locatif" image={previewInvestLocatif}/>
-        <ProjectCard title="MathPrepa" image={previewMathPrepa}/>
+        <ProjectCard title="Investissement Locatif" />
+        <ProjectCard title="MathPrepa" image/>
         <ProjectCard title="R2C Association" image/>
         <ProjectCard title="NextJob"/>
         <ProjectCard title="FastPizza"/>
